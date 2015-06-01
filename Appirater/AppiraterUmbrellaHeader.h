cbc47dcba86f6b176741834c9f58a54309536ce6
dgoon <kslee@hpcnt.com>
2015-06-01 15:48:01 +0900

    프레임웍 빌드용 프로젝트 추가

---
+++ Appirater/AppiraterUmbrellaHeader.h
@@ -0,0 +1,21 @@
+//
+//  Appirater.h
+//  Appirater
+//
+//  Created by dgoon on 2015. 6. 1..
+//  Copyright (c) 2015년 Hyperconnect. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Appirater.
+FOUNDATION_EXPORT double AppiraterVersionNumber;
+
+//! Project version string for Appirater.
+FOUNDATION_EXPORT const unsigned char AppiraterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Appirater/PublicHeader.h>
+
+
+#import "Appirater.h"
+#import "AppiraterDelegate.h"
