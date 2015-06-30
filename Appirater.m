c14104dc8f2b5d5256acec9c465bddb36e84a554
Hoon H <hh@hpcnt.com>
2015-06-30 17:10:19 +0900

    메인번들이 아니라 현재 모듈의 번들에서 리소스를 찾도록 수정.

--- Appirater.m
+++ Appirater.m
@@ -161,14 +161,18 @@ static BOOL _alwaysUseMainBundle = NO;
     if (_alwaysUseMainBundle) {
         bundle = [NSBundle mainBundle];
     } else {
-        NSURL *appiraterBundleURL = [[NSBundle mainBundle] URLForResource:@"Appirater" withExtension:@"bundle"];
-
-        if (appiraterBundleURL) {
-            // Appirater.bundle will likely only exist when used via CocoaPods
-            bundle = [NSBundle bundleWithURL:appiraterBundleURL];
-        } else {
-            bundle = [NSBundle mainBundle];
-        }
+        //  HOTFIX:
+        //  모듈의 번들을 자동으로 찾도록.
+        NSBundle*   selfBundle  =   [NSBundle bundleForClass: [self class]];
+        return      selfBundle;
+//        NSURL *appiraterBundleURL = [[NSBundle mainBundle] URLForResource:@"Appirater" withExtension:@"bundle"];
+//
+//        if (appiraterBundleURL) {
+//            // Appirater.bundle will likely only exist when used via CocoaPods
+//            bundle = [NSBundle bundleWithURL:appiraterBundleURL];
+//        } else {
+//            bundle = [NSBundle mainBundle];
+//        }
     }
 
     return bundle;
